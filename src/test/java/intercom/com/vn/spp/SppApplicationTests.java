33fb572b757d8d0af351cf55b3d803e1d02d0510
Hoang Quang Chien <hoangchien090508@outlook.com.vn>
2023-12-13 23:05:51 +0700

    first commit

---
+++ src/test/java/intercom/com/vn/spp/SppApplicationTests.java
@@ -0,0 +1,13 @@
+package intercom.com.vn.spp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
