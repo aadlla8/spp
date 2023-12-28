19f75614f03ee27c416ad698635793c27132c580
chien <hoangchien090508@yahoo.com>
2023-12-28 23:15:39 +0700

    mysql

--- src/main/java/intercom/com/vn/spp/model/Employee.java
+++ src/main/java/intercom/com/vn/spp/model/Employee.java
@@ -1,7 +1,5 @@
 package intercom.com.vn.spp.model;
 
-import com.fasterxml.jackson.core.sym.Name;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
