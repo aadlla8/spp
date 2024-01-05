5e3309fd82b9f624438ef58cb23c9b013aba78a4
chien <chien@localhost.localdomain>
2024-01-05 09:55:01 -0500

    update

--- src/main/java/intercom/com/vn/spp/controller/UserController.java
+++ src/main/java/intercom/com/vn/spp/controller/UserController.java
@@ -5,14 +5,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired; 
 import org.springframework.security.access.prepost.PreAuthorize; 
-import org.springframework.security.authentication.AuthenticationManager; 
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken; 
-import org.springframework.security.core.Authentication; 
-import org.springframework.security.core.userdetails.UsernameNotFoundException; 
 import org.springframework.web.bind.annotation.*;
 
-import intercom.com.vn.spp.jwtutils.AuthRequest;
-import intercom.com.vn.spp.jwtutils.JwtService;
 import intercom.com.vn.spp.model.UserInfo;
 import intercom.com.vn.spp.services.UserInfoService; 
 
