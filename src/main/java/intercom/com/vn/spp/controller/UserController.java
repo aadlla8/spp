a440298ae2ed8c5443bc2d1d03cfc98cb360745a
chien <chien@localhost.localdomain>
2024-01-05 10:40:20 -0500

    update

--- src/main/java/intercom/com/vn/spp/controller/UserController.java
+++ src/main/java/intercom/com/vn/spp/controller/UserController.java
@@ -1,29 +1,30 @@
 package intercom.com.vn.spp.controller;
 
-
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired; 
-import org.springframework.security.access.prepost.PreAuthorize; 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import intercom.com.vn.spp.model.UserInfo;
-import intercom.com.vn.spp.services.UserInfoService; 
+import intercom.com.vn.spp.services.UserInfoService;
 
 @RestController
 @CrossOrigin()
-@RequestMapping("/api/v1") 
-public class UserController { 
+@RequestMapping("/api/v1")
+public class UserController {
 
 	@Autowired
-	private UserInfoService service;  
+	private UserInfoService service;
+
 	@GetMapping("/users")
-	public List<UserInfo> getAll(){
+	public List<UserInfo> getAll() {
 		return service.getAll();
 	}
-	@PutMapping("/users/{id}") 
-	@PreAuthorize("hasAuthority('ROLE_ADMIN')") 
-	public String update(@RequestBody UserInfo userInfo, @PathVariable(value = "id")  Integer id) { 	 
-		return service.updateUser(userInfo,id); 
-	}  
-} 
+
+	@PutMapping("/users/{id}")
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
+	public String update(@RequestBody UserInfo userInfo, @PathVariable(value = "id") Integer id) {
+		return service.updateUser(userInfo, id);
+	}
+}
